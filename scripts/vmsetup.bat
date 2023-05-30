4b913c2916a47e514fa273aa904d7eb128282e87
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-30 10:23:44 -0500

    vmsetup additions

--- scripts/vmsetup.bat
+++ scripts/vmsetup.bat
@@ -15,7 +15,6 @@ mkdir c:\tools\WTSettings.json C:\Users\student\AppData\Local\Packages\Microsoft
 mkdir c:\tools\WTSettings.json C:\Users\student\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
 copy /y C:\Users\student\AUD507-Labs\config\WTSettings.json C:\Users\student\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
-
 echo Setting default browser to FirefoxESR
 setdefaultbrowser HKLM Firefox-308046B0AF4A39CB
 
@@ -24,7 +23,12 @@ mkdir C:\Users\student\AppData\Roaming\code
 mkdir C:\Users\student\AppData\Roaming\code\user
 copy /y C:\Users\student\AUD507-Labs\config\codeSettings.json c:\users\student\appdata\roaming\code\user\settings.json
 
-
 echo Copying Firefox policies File
 mkdir "c:\Program Files\Mozilla Firefox\distribution"
 copy /y C:\Users\student\AUD507-Labs\config\policies.json "c:\Program Files\Mozilla Firefox\distribution\policies.json" 
+
+echo SSH Setup
+copy /y C:\Users\student\AUD507-Labs\config\known_hosts c:\users\student\.ssh
+copy /y C:\Users\student\AUD507-Labs\config\almakey c:\users\student\.ssh
+copy /y C:\Users\student\AUD507-Labs\config\ubuntukey c:\users\student\.ssh
+copy /y C:\Users\student\AUD507-Labs\config\hosts c:\windows\system32\drivers\etc\hosts
