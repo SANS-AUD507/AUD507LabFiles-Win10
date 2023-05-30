b116e330b1e98c82f12bc9827f83482d55fd33b5
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-30 09:47:04 -0500

    Create VMSetupInstructor.bat

---
+++ instructorResources/VMSetupInstructor.bat
@@ -0,0 +1,7 @@
+call C:\Users\student\AUD507-Labs\scripts\vmsetup.bat
+
+@rem larger fonts for terminal
+copy /y C:\Users\student\AUD507-Labs\instructorResources\WTSettings-instructor.json C:\Users\student\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+
+echo "launching zoomit"
+c:\Users\student\AUD507-Labs\instructorResources\ZoomIt.exe
