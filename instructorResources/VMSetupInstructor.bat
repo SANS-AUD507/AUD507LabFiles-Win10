0872c828c122692f61f174d14919bf7206796d3b
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-30 09:57:12 -0500

    zoomit for instructors

--- instructorResources/VMSetupInstructor.bat
+++ instructorResources/VMSetupInstructor.bat
@@ -3,5 +3,6 @@ call C:\Users\student\AUD507-Labs\scripts\vmsetup.bat
 @rem larger fonts for terminal
 copy /y C:\Users\student\AUD507-Labs\instructorResources\WTSettings-instructor.json C:\Users\student\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
-echo "launching zoomit"
-c:\Users\student\AUD507-Labs\instructorResources\ZoomIt.exe
+reg import c:\Users\student\AUD507-Labs\instructorResources\zoomit.reg
+
+start c:\Users\student\AUD507-Labs\instructorResources\zoomit.exe
