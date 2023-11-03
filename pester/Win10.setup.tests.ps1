919c8aab3e66ef00a299270978e627c3e3edfdab
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-11-03 15:23:23 -0500

    Combined setup tests

--- pester/Win10.setup.tests.ps1
+++ pester/Win10.setup.tests.ps1
@@ -50,8 +50,48 @@ BeforeDiscovery {
   }
 }
 
-Describe 'Lab 1.3 Setup tests for 507Win10 VM' {
-    
+Describe 'Lab Setup tests for 507Win10 VM' {
+  Context 'Network connectivity' {
+    It 'Ping 507Ubuntu - HostOnly' {
+        $res = Test-NetConnection -ComputerName ubuntu
+        $res | Should -BeTrue -Because 'Ensure that second network adapter is set to Host-only'
+    }
+
+    It 'Ping Google - NAT' {
+        $res = Test-NetConnection -ComputerName dns.google
+        $res | Should -BeTrue -Because 'Ensure that first network adapter is set to NAT'
+    }
+  }
+
+  Context 'Local system checks' {
+    It 'Drive free space > 10GB' {
+        (Get-PSDrive -name c).Free | Should -BeGreaterThan 10000000000 -Because 'VM disk is low on space'
+    }
+  }
+
+  #The firefox plugins won't show up in osquery until the application has been run once, and
+  #the polices.json file processed.
+  Context 'Firefox plugins' {
+    BeforeAll {
+        $plugins = osqueryi "select * from firefox_addons;" --json 2>$null | ConvertFrom-Json
+    }
+
+    It 'Retire.js' {
+        $plugins.identifier | Should -Contain '@retire.js' `
+          -Because "Firefox must have been launched once to load addons. Launch Firefox and re-run the tests."
+    }
+
+    It 'Wappalyzer' {
+        $plugins.identifier | Should -Contain 'wappalyzer@crunchlabz.com' `
+          -Because "Firefox must have been launched once to load addons. Launch Firefox and re-run the tests."
+    }
+
+    It 'FoxyProxy' {
+        $plugins.identifier | Should -Contain 'foxyproxy@eric.h.jung' `
+          -Because "Firefox must have been launched once to load addons. Launch Firefox and re-run the tests."
+    }
+  }
+  
   Context 'Cloud services - AWS' -skip:$skipAWS {
     BeforeAll{
       Import-Module AWSPowerShell.NetCore
