329258e691eed64dda07e2ae73b38664c0b5b5da
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-11-08 08:53:34 -0600

    Edit vm setup script for instructors

--- instructorResources/VMSetupInstructor.bat
+++ instructorResources/VMSetupInstructor.bat
@@ -1,5 +1,3 @@
-call C:\Users\student\AUD507-Labs\scripts\vmsetup.bat
-
 @rem larger fonts for terminal
 copy /y C:\Users\student\AUD507-Labs\instructorResources\WTSettings-instructor.json C:\Users\student\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
