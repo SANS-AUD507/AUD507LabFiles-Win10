0aaa54307f77e3dbc2abd4140382903f2018ab19
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-11-17 08:02:02 -0600

    notification disable reg keys in termlink.ps1

--- scripts/termLink.ps1
+++ scripts/termLink.ps1
@@ -34,3 +34,8 @@ $Shortcut.Save()
 # Pin Aud507-Labs Folder to Quick Access
 $shortcut = New-Object -com shell.application
 $shortcut.NameSpace("C:\Users\student\AUD507-Labs").Self.InvokeVerb("pintohome")
+
+# Disable notifications
+New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -force
+New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -PropertyType "DWord" -Value 1
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -PropertyType "DWord" -Value 0
