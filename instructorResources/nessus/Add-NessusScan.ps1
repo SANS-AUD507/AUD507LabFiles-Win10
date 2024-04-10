8a6cd13ee9bcf6aeb0f5c21db59dc1c0f10e0a01
AuditClay <42394776+AuditClay@users.noreply.github.com>
2024-04-09 19:56:11 -0500

    fix json depth for nessus api call

--- instructorResources/nessus/Add-NessusScan.ps1
+++ instructorResources/nessus/Add-NessusScan.ps1
@@ -4,9 +4,9 @@ TODO: Get list of template uuids to get the Basic San one
 #>
 [CmdletBinding()]
 param (
-    [string]$fileName = ".\windowsScan.json",
-    [string]$uuid,
-    [boolean] $launchNow = $true
+  [string]$fileName = ".\windowsScan.json",
+  [string]$uuid,
+  [boolean] $launchNow = $true
 )
 
 #Get an auth token for the local scanner
@@ -19,33 +19,33 @@ $body = @{
 $baseUri = 'https://scanner.5x7.local:8834'
 $uri = $baseUri + '/session'
 
-$res = invoke-RestMethod -SkipCertificateCheck -Method Post `
-  -uri $uri -body $body
+$res = Invoke-RestMethod -SkipCertificateCheck -Method Post `
+  -Uri $uri -Body $body
 "Token obtained: $($res.token)"
 
 # Get the API key that magically makes everything in the API work,
 # even though it shouldn't...
-$js = (Invoke-WebRequest -SkipCertificateCheck -uri $baseUri/nessus6.js).rawContent
+$js = (Invoke-WebRequest -SkipCertificateCheck -Uri $baseUri/nessus6.js).rawContent
 $m = ($js -split ";" ) -match "return`"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
 $apiToken = ($m -replace '.*return"', '') -replace '".*', ''
 
 # Set the headers for authenticated requests
 $headers = @{
   'X-Api-Token' = "$apiToken"
-  'X-Cookie' = "token=$($res.token)"
+  'X-Cookie'    = "token=$($res.token)"
 }
 $headers
 
-$bodyObject =Get-Content $fileName | ConvertFrom-Json
+$bodyObject = Get-Content $fileName | ConvertFrom-Json
 
 # If no uuid is provide by the user, then
 # get the uuid of the "basic" network scan from the scanner
 # and insert it into the json data
 if ( [string]::IsNullOrEmpty($uuid) ) {
   $uri = $baseUri + '/editor/scan/templates'
-  $res = invoke-RestMethod -SkipCertificateCheck -Method Get `
-    -uri $uri -Headers $headers
-  $uuid = ($res.templates | Where-Object Name -eq 'basic').uuid
+  $res = Invoke-RestMethod -SkipCertificateCheck -Method Get `
+    -Uri $uri -Headers $headers
+  $uuid = ($res.templates | Where-Object Name -EQ 'basic').uuid
   $uuid
   $bodyObject.uuid = $uuid
 }
@@ -53,10 +53,10 @@ if ( [string]::IsNullOrEmpty($uuid) ) {
 #Set the launch_now property to match the user parameters
 $bodyObject.settings.launch_now = $launchNow
 
-$body = $bodyObject | ConvertTo-Json -Compress
+$body = $bodyObject | ConvertTo-Json -Compress -Depth 10
 
 # Add the scan 
 
 $uri = $baseUri + "/scans"
-Invoke-RestMethod -uri $uri -SkipCertificateCheck -Method Post `
+Invoke-RestMethod -Uri $uri -SkipCertificateCheck -Method Post `
   -Headers $headers -Body $body -ContentType 'application/json'
