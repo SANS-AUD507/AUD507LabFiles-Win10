53293c6ce0cd3dab2a1c9fa473fb481439717f01
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-03-26 09:17:45 -0500

    burp launcher

--- scripts/launchBurp.cmd
+++ scripts/launchBurp.cmd
@@ -1,1 +1,1 @@
-start C:\tools\burpsuite\jre\bin\javaw.exe -jar C:\tools\burpsuite\burpsuite_community.jar
+start C:\tools\burpsuiteCommunity\jre\bin\javaw.exe -jar C:\tools\burpsuiteCommunity\burpsuite_community.jar
